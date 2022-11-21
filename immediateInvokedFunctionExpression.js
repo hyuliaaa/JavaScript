cf45dae9eb194f6a869fc084b927b00f6acb9a5a
hyuliaaa <hulia_99@abv.bg>
2022-11-21 23:16:37 +0200

    IIFE

---
+++ immediateInvokedFunctionExpression.js
@@ -0,0 +1,8 @@
+//IIFE - imediately invoked function expression
+(function() {
+    console.log("Eto ti funciq")
+})(); // calling in place
+
+//usually used in the beggining of a script
+//when we want to execute something only once
+
